5782eda5328d487e6d2a7b8eef44d1560027ff96
Diego Hernández <diego.hernandez@encora.com>
2023-06-18 00:53:52 -0600

    test:Adds Cucumber test class

---
+++ src/test/kotlin/org/diegoehg/atmapp/RunCucumberTest.kt
@@ -0,0 +1,9 @@
+package org.diegoehg.atmapp
+
+import io.cucumber.junit.CucumberOptions
+import io.cucumber.junit.Cucumber
+import org.junit.runner.RunWith
+
+@RunWith(Cucumber::class)
+@CucumberOptions(plugin = ["pretty"])
+class RunCucumberTest
