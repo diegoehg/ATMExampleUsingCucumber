255119fd1502f53e5ea1606d3ec7acb34417a576
Diego Hernández <diego.hernandez@encora.com>
2023-06-18 00:56:43 -0600

    chore:Adds JUnit engine dependency. There was an error thrown when running tests, it didn't find a test runner.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val kotlin_version = "1.8.22"
 val cucumber_version = "7.12.1"
+val junit_version = "5.10.0-M1"
 
 plugins {
     kotlin("jvm") version "1.8.22"
@@ -19,6 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     testImplementation("io.cucumber:cucumber-java:$cucumber_version")
     testImplementation("io.cucumber:cucumber-junit:$cucumber_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junit_version")
 }
 
 tasks.named<Test>("test") {
