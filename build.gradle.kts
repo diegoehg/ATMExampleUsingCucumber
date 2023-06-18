1161c977b385324b324d57d37c0a9ca854cdeb25
Diego Hernández <diego.hernandez@encora.com>
2023-06-17 23:07:25 -0600

    chore:Adds Cucumber & Kotlin dependencies to Gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotlin_version = "1.8.22"
+val cucumber_version = "7.12.1"
+
 plugins {
     kotlin("jvm") version "1.8.22"
     application
@@ -10,6 +13,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    testImplementation("io.cucumber:cucumber-java:$cucumber_version")
+    testImplementation("io.cucumber:cucumber-junit:$cucumber_version")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
 
 kotlin {
