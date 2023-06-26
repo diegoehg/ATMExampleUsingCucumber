79fc84882592f830542672323ba85786a61ef9b4
Diego Hernández <diegoehg@gmail.com>
2023-06-25 22:06:00 -0600

    build:Deletes unused dependencies Changes jvm to version 11 Uses another idiom to set test task

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,25 +10,23 @@ repositories {
     mavenCentral()
 }
 
-val kotlinVersion = "1.8.22"
 val cucumberVersion = "7.12.1"
 val junitPlatformVersion = "1.9.3"
 val junitJupiterVersion = "5.9.3"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     testImplementation("io.cucumber:cucumber-java:$cucumberVersion")
     testImplementation("io.cucumber:cucumber-junit-platform-engine:$cucumberVersion")
     testImplementation("org.junit.platform:junit-platform-suite:$junitPlatformVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
 
-tasks.named<Test>("test") {
+tasks.test {
     useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(11)
 }
 
 application {
