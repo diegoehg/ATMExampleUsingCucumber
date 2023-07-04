a2438371ffd6101f1141b09c1146e08d8fde51f8
Diego Hernández <diegoehg@gmail.com>
2023-07-04 11:50:01 -0600

    test:Adds new feature and corresponding steps

--- src/test/kotlin/org/diegoehg/atmapp/StepDefinitions.kt
+++ src/test/kotlin/org/diegoehg/atmapp/StepDefinitions.kt
@@ -1,8 +1,14 @@
 package org.diegoehg.atmapp
 
 import io.cucumber.java.en.*
+import org.junit.jupiter.api.Assertions.assertEquals
+
+fun isItFriday(today:String) = "Nope"
 
 class StepDefinitions {
+    private lateinit var today:String
+    private lateinit var actualAnswer:String
+
     @Given("an example scenario")
     fun anExampleScenario() {
     }
@@ -13,5 +19,20 @@ class StepDefinitions {
 
     @Then("the scenario passes")
     fun theScenarioPasses() {
+    }
+
+    @Given("today is Sunday")
+    fun today_is_sunday() {
+        today = "Sunday"
+    }
+
+    @When("I ask whether it's Friday yet")
+    fun i_ask_whether_it_s_friday_yet() {
+        actualAnswer = isItFriday(today)
+    }
+
+    @Then("I should be told {string}")
+    fun i_should_be_told(expectedAnswer: String) {
+        assertEquals(expectedAnswer, actualAnswer)
     }
 }
